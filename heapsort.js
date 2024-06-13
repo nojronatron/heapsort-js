726b298d630eb098918088c6b95b4355fa578aff
Nojronatron <jonrumsey.dev@gmail.com>
2024-06-13 13:21:34 -0700

    implement heapsort class, test init

---
+++ heapsort.js
@@ -0,0 +1,17 @@
+class Heapsort {
+  constructor(inputArray) {
+    this.inputArr = inputArray;
+  }
+  sortV1() {
+    if (this.inputArr.length < 1) {
+      return;
+    }
+    if (this.inputArr.length === 1) {
+      return this.inputArr;
+    }
+
+    return;
+  }
+}
+
+module.exports = Heapsort;
