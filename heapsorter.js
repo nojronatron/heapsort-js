7ed84d110c059105a4008e8fde7d499c20ec500b
Nojronatron <jonrumsey.dev@gmail.com>
2024-06-14 13:04:14 -0700

    implement static heapsort function. tests pass

--- heapsorter.js
+++ heapsorter.js
@@ -68,6 +68,11 @@ class Heapsorter {
     return true;
   }
 
+  /**
+   * Adds a new item to the heap and maintains the heap
+   * property by comparing the new value with its parent.
+   * @param {number} number - The number to be added to the heap.
+   */
   addItemToHeap(number) {
     let num = this.getValueAsNumber(number);
     // add the new value to the end of the heap
@@ -85,6 +90,11 @@ class Heapsorter {
     }
   }
 
+  /**
+   * Removes and returns the top item
+   * while maintaining heap property.
+   * @returns {*} The top item of the heap.
+   */
   removeTopItem() {
     if (this.backingArr.length < 1) {
       return;
@@ -146,6 +156,55 @@ class Heapsorter {
     }
 
     return topItem;
+  }
+
+  // heapsort an input array without using additional storage
+  static heapsort(inputArr) {
+    // validate input is an array
+    if (Array.isArray(inputArr) !== true) {
+      return [];
+    }
+
+    // track decrementing lastIdx
+    for (let lastIdx = inputArr.length - 1; lastIdx > 0; lastIdx--) {
+      // heapify the input array
+      inputArr = Heapsorter.makeHeap(inputArr, lastIdx);
+
+      // swap head and last item
+      let temp = inputArr[lastIdx];
+      inputArr[lastIdx] = inputArr[0];
+      inputArr[0] = temp;
+      // repeat steps until only idx 0 remains
+    }
+    // return the heapsorted array
+    return inputArr;
+  }
+
+  static makeHeap(inpArr, endIdx) {
+    if (endIdx <= 0) {
+      return inpArr;
+    }
+
+    let firstIdx = 0;
+
+    if (Array.isArray(inpArr)) {
+      for (let childIdx = endIdx; childIdx >= firstIdx; childIdx--) {
+        let parentIdx = Math.floor((childIdx - 1) / 2);
+        // work from leaf nodes to root
+        // while (parentIdx >= firstIdx) {
+        // swap if child value greater than parent value
+        if (inpArr[childIdx] > inpArr[parentIdx]) {
+          let tempValue = inpArr[parentIdx];
+          inpArr[parentIdx] = inpArr[childIdx];
+          inpArr[childIdx] = tempValue;
+          // } else {
+          //   break;
+          // }
+        }
+      }
+      return inpArr;
+    }
+    return [];
   }
 
   swapItems(leftIdx, rightIdx) {
